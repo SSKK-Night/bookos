1539cda0f388726bfd8eb50988383b714bf32051
SSKK-Night <zhoupinglibo252@gmail.com>
2025-07-22 05:47:48 +0900

    mutex追加

--- src/mutex.rs
+++ src/mutex.rs
@@ -1,7 +1,41 @@
+use core::cell::UnsageCell;
+use core::ops::{Deref, DerefMut};
+use core::sync::atomic::{AtomicBool, Ordering};
+
 pub struct Mutex<T> {
     locked: AtomicBool,
-    value: T,
+    data: UnsafeCell<T>,
 }
+
+pub struct MutexGuard<'a, T> {
+    lock: &'a Mutex<T>,
+}
+
+impl<'a, T> MutexGuard<'a, T> {
+    lock: &'a Mutex<T>,
+}
+
+impl<'a, T> MutexGuard<'a, T> {
+    fn new(lock: &'a Mutex<T>) -> Self {
+        MutexGuard {
+            lock
+        }
+    }
+}
+
+impl<T> Deref for MutexGurad<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.data.get() }
+    }
+ }
+
+ impl<T> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.unlock();
+    }
+ }
 
 impl<T> Mutex<T> {
     pub fn new(value: T) -> Self {
