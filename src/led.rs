518261522623b12de9af722f3085ed5422f9d0fb
SSKK-Night <zhoupinglibo252@gmail.com>
2025-08-01 04:41:30 +0900

    led追加

--- src/led.rs
+++ src/led.rs
@@ -1,4 +1,28 @@
 use core::ptr::{write_volatile};
+use crate::vcell::VolatileCell;
+
+
+#[repr(C)]
+pub struct PortRegisters {
+    pub dir: VolatileCell<u32>,
+    pub dirclr: VolatileCell<u32>,
+    pub dirset: VolatileCell<u32>,
+    pub dirtgl: VolatileCell<u32>,
+
+    pub out: VolatileCell<u32>,
+    pub outclr: VolatileCell<u32>,
+    pub outset: VolatileCell<u32>,
+    pub outtgl: VolatileCell<u32>,
+
+    pub r#in: VolatileCell<u32>,
+    pub ctrl: VolatileCell<u32>,
+    pub wrconfigf: VolatileCell<u32>,
+    pub evctrl: VolatileCell<u32>,
+
+    pub pmux: [VolatileCell<u8>; 16],
+
+    pub pincfg: [VolatileCell<u8>; 32],
+}
 
 
 pub fn init_led() {
