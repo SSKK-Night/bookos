0dce8201a3cc526da3ea97cb82425dd7c6379457
SSKK-Night <zhoupinglibo252@gmail.com>
2025-09-14 04:17:34 +0900

    syscall.rs追加

--- src/syscall.rs
+++ src/syscall.rs
@@ -9,3 +9,12 @@ pub fn syscall_set_led(value: bool) {
         asm!("svc 0", in("r0") 1, in("r1") value as u32);
     }
 }
+
+pub fn syscall_get_button() -> bool {
+    let result: u32;
+    unsafe {
+        asm!("svc 0", in("r0") 2, lateout("r0") result);
+    }
+
+    result > 0
+}
